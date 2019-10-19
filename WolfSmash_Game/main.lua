59769f7b8e25fe112a74845866a943cba4595c1b
Marcelo Dias <marcelod789@gmail.com>
2019-10-18 22:59:19 -0300

    Joystick e SpriteSheet
    Implementações:
    O jogo reconhece os botões e alavancas de joysticks.
    Player possiu imagem.
    
    Co-Authored-By: kerbeks <kerbeks@users.noreply.github.com>

--- WolfSmash_Game/main.lua
+++ WolfSmash_Game/main.lua
@@ -1,29 +1,31 @@
 require("player")
 
---local world = love.physics.newWorld(0, 650 , true) -- (gravidade no eixo X, Graviade no exio Y, se o corpo pode ficar parado "sleep")
-
 function love.load()
     love.physics.setMeter( 64 ) -- 1 metro = 64 pixels
     world = love.physics.newWorld(0, 9.81 * 64 , true) -- (gravidade no eixo X, Graviade no exio Y, se o corpo pode ficar parado "sleep")
-    world:setCallbacks(beginContact, endContact, nil, nil)
+    world:setCallbacks(beginContact, endContact, nil, nil) --Detecta contatos no mundo
 
-    player0 = newPlayer("player0", world, nil, 325, 325, 400 ,200, 2) --cria um "player" definido no aquivo player.lua
+    joysticks = love.joystick.getJoysticks() -- Pega a lista de Joysticks conectados
 
-    player1 = newPlayer("player1", world, nil, 425, 325, 400 ,200, 10) --cria um "player" definido no aquivo player.lua
+    player0 = newPlayer("player0", world, joysticks[1], "imagens/Spritesheet.png", 325, 325, 400 ,200, 3) --cria um "player" definido no aquivo player.lua
+
+    player1 = newPlayer("player1", world, joysticks[2], "imagens/Spritesheet.png", 425, 325, 400 ,200, 3) --cria um "player" definido no aquivo player.lua
 
 
     objetos = {} --Lista de Objetos
     objetos.chao = {} --Lista de Objetos chão
+    objetos.chao.tag = "chao"
     objetos.chao.body = love.physics.newBody(world, 650/2+9, 650 - 50/2) 
-    objetos.chao.shape = love.physics.newRectangleShape(650, 50)
+    objetos.chao.shape = love.physics.newRectangleShape(1500, 50)
     objetos.chao.fixture = love.physics.newFixture(objetos.chao.body, objetos.chao.shape)
-    objetos.chao.fixture:setUserData("chao")
+    objetos.chao.fixture:setUserData(objetos.chao.tag)
 
     objetos.bloco = {}
+    objetos.bloco.tag = "bloco"
     objetos.bloco.body = love.physics.newBody(world, 200 , 550 , "dynamic")
     objetos.bloco.shape = love.physics.newRectangleShape(0, 0, 50, 100)
     objetos.bloco.fixture = love.physics.newFixture(objetos.bloco.body, objetos.bloco.shape, 2)
-    objetos.bloco.fixture:setUserData("bloco")
+    objetos.bloco.fixture:setUserData(objetos.bloco.tag)
 
     love.graphics.setBackgroundColor(5/255, 155/255, 1)  --Azul - rgb(RED, GREEN, BLUE, ALPHA) só aceita valores entre 0 e 1 para cada campo ex: (255/255, 20/255, 60/255, 1)    
     love.window.setMode(750, 650) -- Deixa a janela com o tamanho indicado
@@ -35,13 +37,13 @@ end
 function love.update( dt )
     world:update( dt )
     
-    player0:setControls("right", "left", "up")
-    player1:setControls("d", "a", "w")
+    player0:setKeyboardControls("right", "left", "up")
+    player1:setKeyboardControls("d", "a", "w")
 
-    player0:update()
-    player1:update()
+    player0:update(dt)
+    player1:update(dt)
     
-    if string.len(text) > 500 then    -- cleanup when 'text' gets too long
+    if string.len(text) > 800 then    -- cleanup when 'text' gets too long
         text = " "
     end
 
@@ -55,11 +57,12 @@ function love.draw()
     love.graphics.setColor( 119/255, 69/255, 22/255) -- Marrom
     love.graphics.polygon("fill", objetos.bloco.body:getWorldPoints( objetos.bloco.shape:getPoints()))
 
-    player0:drawMe(1, 0, 0) --Desenha o player com a cor indicada  --Temporário
-    player1:drawMe(1, 0.5, 1)
+    love.graphics.setColor( 1, 1, 1)
+    player0:drawMySprite()
+    player1:drawMySprite()
 
     textoTemporario()
-    
+
 end
 
 function textoTemporario()
@@ -68,6 +71,96 @@ function textoTemporario()
 
     love.graphics.setColor( 0, 0, 0)  --PRETO
     love.graphics.print(vida, 500, 10)
+
+    love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+
+    -------------TESTE JOYSTICK-----------------------
+    joysticks = love.joystick.getJoysticks()
+    if joysticks[2] then
+        love.graphics.print("teste 1 2 3", 400, 400)
+    end
+    for i, joystick in ipairs(joysticks) do
+        
+        love.graphics.print("-->Joysticks detectados: ", 500, 55)
+        love.graphics.print("\t"..joystick:getName(), 500, i * 10 + 60)
+    end
+
+    if (joysticks[1] ~= nil) then --Se tiver joystick conectado
+        local direcao = joysticks[1]:getAxis(1)
+        if( direcao ~= 0) then
+            love.graphics.print("Analogico esquerdo, Direção do eixo x : "..direcao, 400,  160)
+        end
+        direcao = joysticks[1]:getAxis(2)
+        if( direcao ~= 0) then
+            love.graphics.print("Analogico esquerdo, Direção do eixo y: "..direcao, 400,  175)
+        end
+        direcao = joysticks[1]:getAxis(3)
+        if( direcao ~= -1) then
+            love.graphics.print("LT: "..direcao, 400,  220)
+        end
+        direcao = joysticks[1]:getAxis(4)
+        if( direcao ~= 0) then
+            love.graphics.print("Analogico direito, Direção do eixo x :"..direcao, 400,  190)
+        end
+        direcao = joysticks[1]:getAxis(5)
+        if( direcao ~= 0) then
+            love.graphics.print("Analogico direito, Direção do eixo y :"..direcao, 400,  205)
+        end
+        direcao = joysticks[1]:getAxis(6)
+        if( direcao ~= -1) then
+            love.graphics.print("RT: "..direcao, 450,  220)
+        end
+        local botao = joysticks[1]:isDown(1)
+        if(botao) then
+            love.graphics.print("Y", 400,  280)
+        end
+        botao = joysticks[1]:isDown(2)
+        if(botao) then
+            love.graphics.print("B", 410,  280)
+        end
+        
+        botao = joysticks[1]:isDown(3)
+        if(botao) then
+            love.graphics.print("A", 420,  280)
+        end
+        
+        botao = joysticks[1]:isDown(4)
+        if(botao) then
+            love.graphics.print("X", 430,  280)
+        end
+        
+        botao = joysticks[1]:isDown(5)
+        if(botao) then
+            love.graphics.print("R1", 440,  280)
+        end
+        
+        botao = joysticks[1]:isDown(6)
+        if(botao) then
+            love.graphics.print("R2", 460,  280)
+        end
+        
+        botao = joysticks[1]:isDown(7)
+        if(botao) then
+            love.graphics.print("L1", 480,  280)
+        end
+
+        botao = joysticks[1]:isDown(8)
+        if(botao) then
+            love.graphics.print("L2", 530,  280)
+        end
+        botao = joysticks[1]:isDown(9)
+        if(botao) then
+            love.graphics.print("Select", 530,  280)
+        end
+        botao = joysticks[1]:isDown(10)
+        if(botao) then
+            love.graphics.print("Start", 530,  280)
+            love.event.quit()
+        end
+        
+    end
+    ------------------------------------------
+
 end
 
 function isUnderneath(normalY, tag1, tag2, objeto1, objeto2) --Retorna o objeto que estava por baixo na hora da colisão (normalY é valor Y do vetor Normal da colisão)
@@ -76,10 +169,8 @@ function isUnderneath(normalY, tag1, tag2, objeto1, objeto2) --Retorna o objeto 
             return objeto1  
         elseif(normalY > 0) then
             return objeto2
-        end
-    end
-
-    if (tag1 == objeto2.tag and tag2 == objeto1.tag) then
+        end    
+    elseif (tag1 == objeto2.tag and tag2 == objeto1.tag) then
         if(normalY < 0) then
             return  objeto2  
         elseif(normalY > 0) then
@@ -90,6 +181,7 @@ function isUnderneath(normalY, tag1, tag2, objeto1, objeto2) --Retorna o objeto 
 end
 
 function beginContact(a, b, coll) --Inicio do contato 
+    contato = coll
     local x, y = coll:getNormal()
     local tagA = a:getUserData()
     local tagB = b:getUserData()
@@ -102,23 +194,27 @@ function beginContact(a, b, coll) --Inicio do contato
 
     --condição de pulo Player0
     player0:setIsGrounded(tagA, tagB, "chao", true)
+    player0:setIsGrounded(tagA, tagB, "bloco", true)
     
     --condição de pulo Player1
     player1:setIsGrounded(tagA, tagB, "chao", true)
+    player1:setIsGrounded(tagA, tagB, "bloco", true)
 
     text = text.."\n"..tagA.." colidindo com "..tagB.." / Vetor normal: (" ..x.." , "..y.." )" --Temporário
 end
 
 function endContact(a, b, coll) -- após o contato terminar
+    local x, y = coll:getNormal()
     local tagA = a:getUserData()
     local tagB = b:getUserData()
 
     --Colisão com Player0
     player0:setIsGrounded(tagA, tagB, "chao", false)
+    player0:setIsGrounded(tagA, tagB, "bloco", false)
 
     --Colisão com Player1
     player1:setIsGrounded(tagA, tagB, "chao", false)
-
+    player1:setIsGrounded(tagA, tagB, "bloco", false)
 
     text = text.."\n"..tagA.." parou de colidir com "..tagB --Temporário
 end
--- WolfSmash_Game/player.lua
+++ WolfSmash_Game/player.lua
@@ -1,38 +1,79 @@
+local anim8 = require 'anim8'
 -- Class Player
 Player = {}
 Player.__index = Player 
 
-function newPlayer(tag, world, img, posX, posY, velX ,jumpForce, life)
+function newPlayer(tag, world, joystick, pathImage, posX, posY, velX ,jumpForce, life)
     local p = {}
-    p.tag = tag --Importante para a detecção de Colisões 
-    p.posX = posX --Posição X do Corpo (posição inicial)
-    p.posY = posY --Posição Y do Corpo (posição inicial)
+    p.tag = tag --Importante para a detecção de Colisões
+    p.world = world 
+    p.joystick = joystick
+    p.image = love.graphics.newImage(pathImage)
+    local g = anim8.newGrid(64, 64, p.image:getWidth(), p.image:getHeight())
+    p.animation = {
+        idle = anim8.newAnimation(g('1-4',1), 0.1),
+        jumping = false,
+        jump = anim8.newAnimation(g('1-4',2), 0.1)
+    }
+    p.animation.idle = anim8.newAnimation(g('1-4',1), 0.1)
+    p.animation.jump = anim8.newAnimation(g('1-4',2), 0.1)
     p.velX = velX --Velocidade de locomoção pelo eixo X
     p.jumpForce = jumpForce * -1 --Força do Pulo (está negativo por conta do eixo Y crescer para baixo)
-    p.width = 50 --Largura do Corpo
-    p.height = 60   --Altura do Corpo
     p.life = life --Vida do Player
     p.isAlive = true
     p.isGrounded = false --Está tocando o chão? true = sim 
 
-    p.body = love.physics.newBody(world, p.posX , p.posY, "dynamic") --Cria o corpo dinamico na posição e mundo indicado 
+    p.body = love.physics.newBody(p.world, posX , posY, "dynamic") --Cria o corpo dinamico na posição e mundo indicado 
     p.body:setFixedRotation(true) --Faz o corpo não ficar girando 
-    p.shape = love.physics.newRectangleShape(0, 0, p.width, p.height)
+    p.shape = love.physics.newRectangleShape(0, 0, 64,64)
     p.fixture = love.physics.newFixture(p.body, p.shape, 1)
-    p.fixture:setRestitution(0.5) --Faz o objeto quicar
+    --p.fixture:setRestitution(0.5) --Faz o objeto quicar
     p.fixture:setUserData(p.tag) --Importante para a detecção de Colisões 
+
+    p.animation.current = p.animation.idle
     
     return setmetatable(p, Player) --Retorna uma instância da Classe Player
 end
 
-function Player:update()
+function Player:update(dt)
+    self.animation.current:update(dt)
+    
+    --------Verifica se está morto----------
     if self.life <= 0 then --Se a vida do player for menor ou igual a 0
         self.isAlive = false 
         self.body:setActive(false) --Desativa o Player quando estiver morto
     end
+    ----------------------------------------
+
+    ---------------Animação-----------------
+    if self.isGrounded then
+        self.animation.current = self.animation.idle
+    else
+        if self.animation.jumping then
+            self.animation.current = self.animation.jump
+        end
+    end
+    ----------------------------------------
+
+    -------Joystick------------
+    if self.joystick ~= nil then -- Se tiver um joystick atrelado ao player
+        local direcaoX = self.joystick:getAxis(1)
+        if direcaoX ~= 0 then
+                self.body:applyForce(self.velX * direcaoX, 0)
+        end
+
+        local botaoA = self.joystick:isDown(1)
+        if botaoA then
+            if self.isGrounded then
+                self.body:applyLinearImpulse(0, self.jumpForce)
+                self.animation.jumping = true
+            end
+        end
+    end
+    -----------------------------
 end
 
-function Player:setControls(right, left, up)--Definir os controles a partir do nome do botão indicado
+function Player:setKeyboardControls(right, left, up)--Definir os controles a partir do nome do botão indicado
     if love.keyboard.isDown(right) then
         self.body:applyForce(self.velX, 0)
     elseif love.keyboard.isDown(left) then
@@ -41,7 +82,14 @@ function Player:setControls(right, left, up)--Definir os controles a partir do n
     if love.keyboard.isDown(up) then
         if self.isGrounded then
             self.body:applyLinearImpulse(0, self.jumpForce)
+            self.animation.jumping = true
         end
+    end
+end
+
+function Player:drawMySprite()
+    if  self.isAlive then --Se o Player estiver vivo ele será desenhando na tela
+        self.animation.current:draw(self.image, self.body:getX() - 32, self.body:getY()-32)
     end
 end
 
@@ -62,4 +110,21 @@ function Player:setIsGrounded(collisionTag1, collisionTag2, tagGround, value) --
             self.isGrounded = value
         end
     end
+end
+
+function Player:touchingOver(collisionTag1, collisionTag2, normalY) --Função verifica se o player está tocando o objeto por cima
+    if (collisionTag1 == self.tag) then        
+        if(normalY < 0) then
+            return false
+        elseif(normalY > 0) then
+            return true
+        end    
+    elseif (collisionTag2 == self.tag) then
+        if(normalY < 0) then
+            return  true 
+        elseif(normalY > 0) then
+             return false 
+        end
+    end
+    return nil
 end
